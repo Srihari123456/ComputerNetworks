24c43fb6d15cbe7744bc2900d3d0a5276f9a4404
Srihari123456 <43612273+Srihari123456@users.noreply.github.com>
2020-10-19 19:37:16 +0530

    Week4 Persistent code update

--- week4-PersistentAndNonPersistentHTTP/persistent/client.c
+++ week4-PersistentAndNonPersistentHTTP/persistent/client.c
@@ -1,41 +1,97 @@
-#include<stdio.h>
-#include<sys/socket.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-int main(){
-        struct sockaddr_in cli;
-        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if(sockfd < 0){
-                printf("Error in creating socket.");
-                return 0;
-        }
-        printf("\nCreation Success\n");
-        cli.sin_family = AF_INET;
-        cli.sin_port = htons(4000);
-        cli.sin_addr.s_addr = inet_addr("127.0.0.1");
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-        int cli_connect = connect(sockfd, (struct sockaddr *)&cli, sizeof(cli));
-        if(cli_connect<0){
-                printf("\nServern connection failed\n.");
-                return 0;
-        }
-        printf("\nConnection Success\n");
+#define MAX 500
+#define PORT 8080
+#define SA struct sockaddr
 
-        while(1){
-                char name[40],readmsg[1000],get_header[1000];
-                printf("Enter filename: ");
-                scanf("%s",name);
-                printf("\n");
-                strcpy(get_header,"");
-                strcat(get_header,"GET: ");
-                strcat(get_header,name);
-                printf("%s",get_header);
-                printf("\n");
-                write(sockfd,name,sizeof(name));
-                write(sockfd,get_header,sizeof(get_header));
-                read(sockfd,readmsg,sizeof(readmsg));
-                printf("%s",readmsg);
-        }
+struct request
+{
+	char choice;
+	char request[MAX];
+};
+
+
+int response_send(int sockfd)
+{
+	
+	char buff[MAX];
+	int n;
+	char choice;
+
+	struct request input;
+		
+	while(1)
+	{	
+		printf("Do you wish to send an HTTP Request?: ");
+		choice = getchar();
+		getchar();
+		
+		input.choice = choice;
+		
+		if (choice != '1')
+		{
+			strcpy(input.request,"None");
+			printf("Exiting...\n");
+			write(sockfd, &input, sizeof(input));
+			return 0;
+		}
+
+		bzero(buff, sizeof(buff));
+		printf("Enter HTTP Request: ");
+		n = 0;
+		while (( buff[n++] = getchar()) != '\n');
+	
+		strcpy(input.request,buff);	
+		write(sockfd, &input, sizeof(input));
+
+		bzero(buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));
+	
+		printf("\nServer Response: \n~\n%s~\n",buff);
+
+		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+	}
+	return 0;
+}
+
+
+int main()
+{
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{		
+		printf("Socket creation failed.\n");
+		exit(0);
+	}
+	printf("Creation Success!\n");
+	
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
+
+	if (( connect(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0)
+	{
+		printf("Socket connection failed.\n");
+		exit(0);
+	}
+	printf("Connection Success!\n\n");
+
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
+	response_send(sockfd);
+	close(sockfd);
+	
+	printf("\n\nConnection Closed.\n");
 }
--- week4-PersistentAndNonPersistentHTTP/persistent/server.c
+++ week4-PersistentAndNonPersistentHTTP/persistent/server.c
@@ -1,65 +1,144 @@
-#include<unistd.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<time.h>
-#include<string.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <ctype.h>
+
+#define MAX 500
+#define PORT 8080
+#define SA struct sockaddr
+
+struct request
+{
+	char choice;
+	char request[MAX];
+};
+
+
+int request_handler(int connfd)
+{
+	char buff[MAX];
+	char object[MAX]="";
+	int n,i,j;
+	char choice;	
+	struct request input;
+	
+	while(1)
+	{
+		if ( read(connfd, &input, sizeof(input)))
+		{
+			choice = input.choice;
+			strcpy(buff, input.request);			
+
+			printf("Request: %s\n",buff);
+
+			if ( choice != '1')
+			{
+				printf("Exiting...\n");
+				break;
+			}
+			
+			int len = strlen(buff);
+			for(i=4,j=0; i<(len-1); i++,j++)
+			{
+					object[j] = buff[i];	
+			}
+	
+			FILE *fd;
+	
+			char filename[MAX]="../Objects/";
+			char message[MAX], content[MAX]="";
+		
+			strcat(content, "REQUEST TIME: ");
+			time_t rawtime;
+			struct tm * timeinfo;
+	
+			time (&rawtime);
+			timeinfo = localtime(&rawtime);
+	
+			strcat(content, asctime(timeinfo));
+			strcat(content, "Content: \n");
+
+			strcat(filename,object);
+			fd = fopen(filename,"r+");
+
+			if (fd == NULL)
+			{
+				printf("Object does not exist\n");
+				return 1;
+			}
+		
+			while ( fgets(message,MAX,fd) != NULL )
+			{
+				strcat(content,message);
+			}		
+		
+			fclose(fd);
+	
+			printf("Requested Object: \n~\n%s~\n",content);
+	
+			printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+			write(connfd, content, sizeof(content));
+		}
+	}
+	return 0;
+}
+
 int main()
 {
-        struct sockaddr_in serveraddr;
-        int listensockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if(listensockfd < 0){
-                printf("Error in creating socket.");
-                return 0;
-        }
-        printf("\nCreation Success\n");
-        serveraddr.sin_family = AF_INET;
-        serveraddr.sin_port = htons(4000);
-        serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli;
 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{		
+		printf("Socket creation failed.\n");
+		exit(0);
+	}
+	printf("Creation Success!\n");
+	
+	bzero(&servaddr, sizeof(servaddr));
 
-        if(bind(listensockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) != 0){
-                printf("Error in binding.");
-                return 0;
-        }
-        printf("\nBinding success\n");
-        int server_listen = listen(listensockfd,10);
-        if(server_listen == -1){
-                printf("Error in listening.");
-                return 0;
-        }
-        printf("\nListen Success\n");
-        int connection = accept(listensockfd, (struct sockaddr*)NULL, NULL);
-        if(connection < 0){
-                printf("\nServer accept failed.\n");
-                return 0;
-        }
-        printf("\nAccess success \n");
-        while(1){
-                char fn[1000],readmsg[1000],headermsg[1000],res[1000];
-                read(connection,fn,sizeof(fn));
-                read(connection,headermsg,sizeof(headermsg));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
 
-                printf("HTTP Request: ");
-                printf("%s",headermsg,sizeof(headermsg));
-                printf("\n");
-                FILE *fp;
-                fp = fopen(fn,"r");
+	if (( bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0)
+	{
+		printf("Socket binding failed.\n");
+		exit(0);
+	}
+	printf("Binding Success!\n");
 
-                fread(readmsg,1000,1,fp);
-                char timedate[1000];
-                time_t t = time(NULL);
-                struct tm *tm = localtime(&t);
-                strcpy(timedate,asctime(tm));
-                printf("Current time: %s \n", timedate);
-                printf("Content of %s: %s",fn,readmsg);
+	if (( listen(sockfd, 5)) != 0)
+	{
+		printf("Listen Failed \n");
+		exit(0);
+	}
+	printf("Listen Success!\n");
+	
+	len = sizeof(cli);
+	connfd = accept(sockfd, (SA*)&cli, &len);
+	if (connfd < 0)
+	{
+		printf("Server accept failed.\n");
+		exit(0);
+	}
+	else
+	{
+		printf("Accept Success!\n\n");
+	}
 
-                strcpy(res,"");
-                strcat(res,"HTTP RESPONSE MESSAGE: ");
-                strcat(res,"\nTimestamp: ");
-                strcat(res,timedate);
-                strcat(res,"Message: ");
-                strcat(res,readmsg);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
-                write(connection,res,sizeof(res));
-        }
+	request_handler(connfd);
+
+	close(sockfd);
+	printf("\n\nConnection Closed.\n");
+	
 }
